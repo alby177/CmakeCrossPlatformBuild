23fb94142a433101ba8de4613e280c38b7506b48
Alberto Gotta <alberto.gotta@spea.com>
2025-06-12 20:55:01 +0200

    Update build script for Windows fixing Visual Studio solution creation and files clean

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import sys, subprocess, platform
 
 actions = {"Linux": {"build": "cmake -S. -Bbuild", "clean": "rm -rf build bin"}, 
-           "Windows": {"build": 'cmake -g "Visual Studio 17 2022"', "clean": ""}}
+           "Windows": {"build": 'cmake -G "Visual Studio 17 2022" -Bbuild', "clean": "rmdir build bin /s /q"}}
 
 if len(sys.argv) > 1:
   actionsForSystem = actions[platform.system()]
