581f3ba2c63c2b8cd385fcb6cdddb375fa4b8320
Alberto Gotta <gotta.alberto@gmail.com>
2025-06-16 23:52:06 +0200

    Added support for MAC build

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import sys, subprocess, platform
 
 actions = {"Linux": {"build": "cmake -S. -Bbuild", "clean": "rm -rf build bin"}, 
+           "Darwin": {"build": "cmake -S. -Bbuild", "clean": "rm -rf build bin"}, 
            "Windows": {"build": 'cmake -G "Visual Studio 17 2022" -Bbuild', "clean": "rmdir build bin /s /q"}}
 
 if len(sys.argv) > 1:
