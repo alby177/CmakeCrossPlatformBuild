881c9f2b71527d3a9a6070198b44226b6bd96a2c
Alberto Gotta <gotta.alberto@gmail.com>
2025-06-21 12:13:39 +0200

    Add GenericLibrary

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-
-auto sum(int a, int b)
-{
-  return a + b;
-}
+#include "GenericLibrary/GenericLibrary.h"
 
 int main(int argc, char** argv)
 {
   std::cout << "We are on a " << CMAKEPROVA << " machine" << std::endl;
-  std::cout << sum(8, 5) << std::endl;
+  std::cout << AddNumbers(8, 5) << std::endl;
   return 0;
 }
